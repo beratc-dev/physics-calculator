1f1ba4cc1cc1d20aea579f5d2d6a2df43fe8654b
Berat Cetin <berat.cetin.dev@gmail.com>
2026-01-16 23:40:56 +0300

    Update physics_calculator.py

--- physics_calculator.py
+++ physics_calculator.py
@@ -2,8 +2,12 @@ def calculate_speed():
     distance = float(input("Enter distance (meters): "))
     time = float(input("Enter time (seconds): "))
     
-    if time == 0:
-        print("Time cannot be zero.")
+    if time <= 0:
+        print("Time cannot be zero or less.")
+    elif distance == 0:
+        print("Undefined calculate.")
+    elif distance < 0:
+        print("Distance cannot be less than zero.")
     else:
         speed = distance / time
         print("Speed:", speed, "m/s")
@@ -24,8 +28,8 @@ def calculate_force():
     mass = float(input("Enter mass (kg): "))
     acceleration = float(input("Enter acceleration (m/s²): "))
     
-    if mass == 0:
-        print("An object cannot have zero mass.")
+    if mass <= 0:
+        print("Mass must be greater than zero.")
     elif acceleration == 0:
         print("Acceleration is zero, force will be zero.")
     else:
@@ -52,8 +56,8 @@ def calculate_density():
 
     if volume == 0:
         print("Volume cannot be zero for a physical object.")
-    elif mass == 0:
-        print("An object cannot have zero mass.")
+    elif mass <= 0:
+        print("Mass must be greater than zero.")
     else:
         density = mass / volume
         print("Density:", density, "kg/m³")
@@ -75,8 +79,8 @@ def calculate_potential_energy():
     height = float(input("Enter height (m): "))
     gravity = 9.81
     
-    if mass == 0:
-        print("An object cannot have zero mass.")
+    if mass <= 0:
+        print("Mass must be greater than zero.")
     else:
         potential_energy = mass * gravity * height
         print("Potential Energy:", potential_energy, "J" )
@@ -86,8 +90,8 @@ def calculate_kinetic_energy():
     mass = float(input("Enter mass (kg): "))
     velocity = float(input("Enter velocity (m/s): "))
 
-    if mass == 0:
-        print("An object cannot have zero mass.")
+    if mass <= 0:
+        print("Mass must be greater than zero.")
     else:
         kinetic_energy = 0.5 * mass * velocity ** 2
         print("Kinetic Energy:", kinetic_energy, "J" )
@@ -97,11 +101,22 @@ def calculate_momentum():
     mass = float(input("Enter mass (kg): "))
     velocity = float(input("Enter velocity (m/s): "))
 
-    if mass == 0:
-        print("An object cannot have zero mass.")
+    if mass <= 0:
+        print("Mass must be greater than zero.")
     else:
         momentum = mass * velocity
         print("Momentum:", momentum, "p")
+
+
+def calculate_thermodynamic_entropy():
+    reversible_heat = float(input("Enter reversible heat (J): "))
+    absolute_temperature = float(input("Enter temperature (K): "))
+
+    if absolute_temperature <= 0:
+        print("Absolute temperature must be greater than zero.")
+    else:
+        change_in_entropy = reversible_heat / absolute_temperature
+        print("Change in entropy:", change_in_entropy, "J/K")
 
 
 print("Physics Calculator")
@@ -114,8 +129,9 @@ print("6 - Calculate Pressure")
 print("7 - Calculate Potential Energy")
 print("8 - Calculate Kinetic Energy")
 print("9 - Calculate Momentum")
+print("10 - Calculate Change In Entropy")
 
-choice = input("Choose an option (1, 2, 3, 4, 5, 6, 7, 8, or 9): ")
+choice = input("Choose an option (1, 2, 3, 4, 5, 6, 7, 8, 9, 10): ")
 
 if choice == "1":
     calculate_speed()
@@ -135,5 +151,7 @@ elif choice == "8":
     calculate_kinetic_energy()
 elif choice == "9":
     calculate_momentum()
+elif choice == "10":
+    calculate_thermodynamic_entropy()
 else:
     print("Invalid choice")
